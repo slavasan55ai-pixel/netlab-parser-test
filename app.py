e317d7418b0b305a64aab51650cfc579a5d31ce0
slavasan55ai-pixel <slavasan55.ai@gmail.com>
2025-12-13 22:43:52 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+netlab-parser-test/
+│
+├─ app.py                # Web-сервер + мини-дашборд
+├─ parser_netlab.py      # Скрипт для обращения в API netlab.ru
+├─ requirements.txt      # Зависимости
+└─ README.md             # Инструкция, что делать на Render
